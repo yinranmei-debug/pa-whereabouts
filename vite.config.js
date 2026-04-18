f5cb2059d6248b7903928a0fff68a68c2b91f246
梅茵然 <meiyinran@Mays-MacBook-Pro.local>
2026-04-18 20:44:50 +0800

    feat: complete calendar drag demo and redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite' // 👈 就是少了这一行！
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 确保这个还在，对上传 SharePoint 至关重要
+  base: './',
 })
